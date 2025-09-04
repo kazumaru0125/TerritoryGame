4bcbb80718d3df07a871c5aee8753202a80b1f0f
okayakusan <0fu7580z3854d0p@au.com>
2025-09-04 15:29:01 +0900

    ジャンプの仕様を少し変更

--- Assets/UnityChan/SD_unitychan/Models/Jump.cs
+++ Assets/UnityChan/SD_unitychan/Models/Jump.cs
@@ -1,35 +1,29 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
 public class Jump : MonoBehaviour
 {
-    float jumpForce = 4;
+    float jumpForce = 5;
     bool isJump, isJumpWait; //ジャンプフラグの設定。Unityちゃんが飛んでいるか否か。
     Animator anim; //Unityちゃんのジャンプ設定するためのAnimator
     float jumpWaitTimer; //ジャンプ待機時間
     bool isGrounded;  // 地面にいるか
-
     // Start is called before the first frame update
     void Start()
     {
         //UntiyちゃんのAnimatorを取得する。
         anim = GetComponent<Animator>();
     }
-
     // Update is called once per frame
     void Update()
     {
-        // 地面判定（例：Raycastを使用して足元をチェックする）
         isGrounded = Physics.Raycast(transform.position, Vector3.down, 0.1f);
-
-        if (Input.GetKeyUp("space") && isGrounded && !isJumpWait)
+        if (Input.GetKeyDown("space") && isGrounded && !isJumpWait)
         {
             anim.Play("Jump", 0, 0);
             isJumpWait = true;
             jumpWaitTimer = 0.5f;
         }
-
         if (isJumpWait)
         {
             jumpWaitTimer -= Time.deltaTime;
