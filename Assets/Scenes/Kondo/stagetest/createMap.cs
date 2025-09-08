0bb0e6d8ac04d9c0d734592302c23dce41132237
harchitakemaro <harchi.takemaro@gmail.com>
2025-09-08 14:26:26 +0900

    マップできたー

--- Assets/Scenes/Kondo/stagetest/createMap.cs
+++ Assets/Scenes/Kondo/stagetest/createMap.cs
@@ -19,6 +19,9 @@ public class createMap : MonoBehaviour
     private GameObject lowwall;
     [SerializeField]
     private GameObject nomalwall;
+    [SerializeField]
+    private GameObject floor;
+
 
     // Start is called before the first frame update
     void Start()
@@ -27,7 +30,9 @@ public class createMap : MonoBehaviour
         print(textLines);
 
         // 改行でデータを分割して配列に代入
-        textData = textLines.Split('\n');
+        //textData = textLines.Split('\n');
+        // 改行でデータを分割（空行は無視）
+        textData = test.text.Split(new[] { '\n', '\r' }, System.StringSplitOptions.RemoveEmptyEntries);
 
         // 行数と列数の取得
         yokoNumber = textData[0].Split(',').Length;
@@ -45,24 +50,44 @@ public class createMap : MonoBehaviour
 
             for (int j = 0; j < yokoNumber; j++)
             {
-                testMap[i, j] = tempWords[j];
-
-                if (testMap[i, j] != null)
+                if (j < tempWords.Length) // ← 安全チェック
                 {
-                    switch (testMap[i, j])
+                    testMap[i, j] = tempWords[j];
+
+                    if (testMap[i, j] != null)
                     {
-                        case "1":
-                            Instantiate(hiwall, new Vector3(-4.5f + j, 1.0f, 4.5f - i), Quaternion.identity);
-                            break;
+                        switch (testMap[i, j])
+                        {
+                            case "1":
+                                Instantiate(hiwall, new Vector3(-4.5f + j, 1.0f, 4.5f - i), Quaternion.identity);
+                                //Instantiate(floor, new Vector3(-4.5f + j, -0.5f, 4.5f - i), Quaternion.identity);
+                                break;
 
-                        case "2":
-                            Instantiate(nomalwall, new Vector3(-4.5f + j, 0.5f, 4.5f - i), Quaternion.identity);
-                            break;
+                            case "2":
+                                Instantiate(nomalwall, new Vector3(-4.5f + j, 0.5f, 4.5f - i), Quaternion.identity);
+                                //Instantiate(floor, new Vector3(-4.5f + j, -0.5f, 4.5f - i), Quaternion.identity);
+                                break;
 
-                        case "3":
-                            Instantiate(lowwall, new Vector3(-4.5f + j, 0.25f, 4.5f - i), Quaternion.Euler(90, 0, 0));
-                            break;
+                            case "3":
+                                Instantiate(lowwall, new Vector3(-4.5f + j, 0.25f, 4.5f - i), Quaternion.Euler(90, 0, 0));
+                                //Instantiate(floor, new Vector3(-4.5f + j, -0.5f, 4.5f - i), Quaternion.identity);
+                                break;
+
+                            case "4":
+                                Instantiate(floor, new Vector3(-4.5f + j, -0.5f, 4.5f - i), Quaternion.identity);
+                                break;
+
+                            case "0":
+                                Instantiate(floor, new Vector3(-4.5f + j, -0.5f, 4.5f - i), Quaternion.identity);
+                                break;
+
+                            case "99":
+                                Instantiate(floor, new Vector3(-4.5f + j, -0.5f, 4.5f - i), Quaternion.identity);
+                                break;
+                        }
+                        Instantiate(floor, new Vector3(-4.5f + j, -0.5f, 4.5f - i), Quaternion.identity);
                     }
+                    //Instantiate(floor, new Vector3(-4.5f + j, -0.5f, 4.5f - i), Quaternion.identity);
                 }
             }
         }
