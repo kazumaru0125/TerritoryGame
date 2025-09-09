3c0684554460c23072d9d9f5ffaaac31cc4abb1e
kazumaru0125 <s.kazuma125@gmail.com>
2025-09-09 15:02:41 +0900

    Playerのニックネームをプレイシーンでも表示できるようにした(今はまだ一人分)

--- Assets/Script/Player/PlayerSpawner.cs
+++ Assets/Script/Player/PlayerSpawner.cs
@@ -1,10 +1,24 @@
 using UnityEngine;
 using Photon.Pun;
+using TMPro;
 
 public class PlayerSpawner : MonoBehaviourPunCallbacks
     {
+    public TMP_Text playerNicNameText;
     void Start()
         {
+        string playerName = PhotonNetwork.LocalPlayer.NickName;
+        Debug.Log("プレイヤー名: " + playerName);
+        // --- RoomInfoText ---
+        if (playerNicNameText != null)
+            {
+            playerNicNameText.text = playerName;
+
+            }
+        else
+            {
+            Debug.LogWarning("playerNicNameText が Inspector に設定されていません！");
+            }
         if (PhotonNetwork.InRoom) // すでにルームにいる場合
             {
             SpawnPlayer();
