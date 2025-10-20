b040c2b0fd72dabe1afa67346edece112a7400e7
kazumaru0125 <s.kazuma125@gmail.com>
2025-10-20 12:50:43 +0900

    電気床の仮追加

--- Assets/Script/Player/GetAitem.cs
+++ Assets/Script/Player/GetAitem.cs
@@ -25,7 +25,7 @@ public class GetAitem : MonoBehaviour
     void Update()
         {
         // Spaceキーで効果発動
-        if (Input.GetKeyDown(KeyCode.Space))
+        if (Input.GetKeyDown(KeyCode.X))
             {
             int num = ItemRouletteScript.decidedItemNumber;
 
