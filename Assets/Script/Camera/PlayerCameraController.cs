8d41afacdbe8ff2a566d69a398753a61ad326caf
kazumaru0125 <s.kazuma125@gmail.com>
2025-10-27 14:02:14 +0900

    鬼と人が切り替わるようにした

--- Assets/Script/Camera/PlayerCameraController.cs
+++ Assets/Script/Camera/PlayerCameraController.cs
@@ -7,8 +7,17 @@ public class PlayerCameraController : MonoBehaviourPun
         {
         if (photonView.IsMine)
             {
-                // 自分のプレイヤーをカメラに登録
-                Camera.main.GetComponent<PlayerCameraFollow>().SetTarget(transform);
+            // 自分のプレイヤーをカメラに登録
+            Camera.main.GetComponent<PlayerCameraFollow>().SetTarget(transform);
+            }
+        }
+    private void OnEnable()
+        {
+        // 自分のキャラクターがアクティブ化されたらカメラを登録
+        if (photonView.IsMine)
+            {
+            Debug.Log($"[PlayerCameraController] {gameObject.name} がアクティブになりました。");
+            CameraManager.TryRegisterCamera(this);
             }
         }
     }
