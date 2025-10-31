60bf4519bb2278d1a9d748f756e6bb09a2a5aa43
harchitakemaro <harchi.takemaro@gmail.com>
2025-10-31 12:31:45 +0900

    ミニマップカメラの追従並びに切り替え完成

--- Assets/Scenes/Kondo/newtestmap/CCamera.cs
+++ Assets/Scenes/Kondo/newtestmap/CCamera.cs
@@ -11,19 +11,19 @@ public class CCamera : MonoBehaviour
         fixedY = transform.position.y;
     }
 
-    void Update()  // プレイヤーと同じタイミングで動かす
-    {
-        // 入力されたキーから移動量を取得（testOBJと同じ処理）
-        float moveX = Input.GetAxis("Horizontal");  // ← →
-        float moveZ = Input.GetAxis("Vertical");    // ↑ ↓
+    //void Update()  // プレイヤーと同じタイミングで動かす
+    //{
+    //    // 入力されたキーから移動量を取得（testOBJと同じ処理）
+    //    float moveX = Input.GetAxis("Horizontal");  // ← →
+    //    float moveZ = Input.GetAxis("Vertical");    // ↑ ↓
 
-        // 入力に速さと時間をかけて移動ベクトルを作成
-        Vector3 move = new Vector3(moveX, 0.0f, moveZ) * moveSpeed * Time.deltaTime;
+    //    // 入力に速さと時間をかけて移動ベクトルを作成
+    //    Vector3 move = new Vector3(moveX, 0.0f, moveZ) * moveSpeed * Time.deltaTime;
 
-        // カメラの位置をプレイヤーと同じ量だけ移動させる
-        transform.position += new Vector3(move.x, 0.0f, move.z);
+    //    // カメラの位置をプレイヤーと同じ量だけ移動させる
+    //    transform.position += new Vector3(move.x, 0.0f, move.z);
 
-        // 高さ（Y）は固定しておく
-        transform.position = new Vector3(transform.position.x, fixedY, transform.position.z);
-    }
+    //    // 高さ（Y）は固定しておく
+    //    transform.position = new Vector3(transform.position.x, fixedY, transform.position.z);
+    //}
 }
