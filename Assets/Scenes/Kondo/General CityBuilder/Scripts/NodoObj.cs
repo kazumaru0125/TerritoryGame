eb5c96cc58d5f0e29168fed68f5ab445b719d7eb
harchitakemaro <harchi.takemaro@gmail.com>
2025-11-11 12:35:39 +0900

    不要物爆撃

--- Assets/Scenes/Kondo/General CityBuilder/Scripts/NodoObj.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class NodoObj : MonoBehaviour
-{
-    public Vector3 pos;
-    public List<NodoObj> conex;
-}
