9f5b37796750b5dc9f7e13c9663adbe83e31b6f5
harchitakemaro <harchi.takemaro@gmail.com>
2025-11-12 12:18:23 +0900

    スタート地点のy座標上昇

--- Assets/Scenes/Kondo/newtestmap/createMaps.cs
+++ Assets/Scenes/Kondo/newtestmap/createMaps.cs
@@ -99,7 +99,7 @@ public class createMaps : MonoBehaviour
                                 break;
 
                             case "5"://スタート地点
-                               Instantiate(starts, new Vector3((-4.5f + j) * scale, 0.0f, (4.5f - i) * scale), Quaternion.identity);
+                               Instantiate(starts, new Vector3((-4.5f + j) * scale, 0.2f, (4.5f - i) * scale), Quaternion.identity);
                                break;
                                 
                                 case "6"://お札
