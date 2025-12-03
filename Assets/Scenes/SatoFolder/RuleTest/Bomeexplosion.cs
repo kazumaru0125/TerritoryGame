9390aea6e4719ac80b48fe03257ba995b53509af
kazumaru0125 <s.kazuma125@gmail.com>
2025-12-03 12:14:36 +0900

    爆弾爆発作成

---
+++ Assets/Scenes/SatoFolder/RuleTest/Bomeexplosion.cs
@@ -0,0 +1,51 @@
+using UnityEngine;
+using Photon.Pun;
+
+public class Bomeexplosion : MonoBehaviourPun
+    {
+    public ParticleSystem FastFire;
+    public ParticleSystem Explosion;
+    public GameObject HitBox;
+
+    public bool ExplosionFlag;
+
+    void Start()
+        {
+        Explosion.Stop();
+        ExplosionFlag = false;
+        HitBox.SetActive(false);
+
+        if (photonView.IsMine)   // 所有者のみタイマー処理
+            {
+            StartCoroutine(ExplosionRoutine());
+            }
+        }
+
+    private System.Collections.IEnumerator ExplosionRoutine()
+        {
+        yield return new WaitForSeconds(2f);
+
+        photonView.RPC(nameof(RPC_ExplosionStart), RpcTarget.All);
+
+        yield return new WaitForSeconds(2f);
+
+        photonView.RPC(nameof(RPC_DestroySelf), RpcTarget.All);
+        }
+
+    [PunRPC]
+    void RPC_ExplosionStart()
+        {
+        ExplosionFlag = true;
+
+        if (FastFire != null) FastFire.Stop();
+        if (Explosion != null) Explosion.Play();
+        if (HitBox != null) HitBox.SetActive(true);
+        }
+
+    [PunRPC]
+    void RPC_DestroySelf()
+        {
+        if (HitBox != null) HitBox.SetActive(false);
+        Destroy(gameObject);
+        }
+    }
