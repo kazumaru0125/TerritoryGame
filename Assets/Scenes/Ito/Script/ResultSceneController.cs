a22df6d8fa995c6dc8d8010f758588089f30719a
kazumaru0125 <s.kazuma125@gmail.com>
2025-12-26 11:56:14 +0900

    ループさせれるようにして

--- Assets/Scenes/Ito/Script/ResultSceneController.cs
+++ Assets/Scenes/Ito/Script/ResultSceneController.cs
@@ -1,20 +1,38 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using Photon.Pun;
 
-public class ResultSceneController : MonoBehaviour
-{
-    void Update()
+public class ResultSceneController : MonoBehaviourPunCallbacks
     {
-        // キーボード Space
-        bool space = Input.GetKeyDown(KeyCode.Space);
+    private bool isProcessing = false;
 
-        // Xbox コントローラー A/B
-        bool padA = Input.GetKeyDown(KeyCode.JoystickButton0); 
-        bool padB = Input.GetKeyDown(KeyCode.JoystickButton1); 
+    void Update()
+        {
+        if (isProcessing) return;
+
+        bool space = Input.GetKeyDown(KeyCode.Space);
+        bool padA = Input.GetKeyDown(KeyCode.JoystickButton0);
+        bool padB = Input.GetKeyDown(KeyCode.JoystickButton1);
 
         if (space || padA || padB)
+            {
+            isProcessing = true;
+
+            if (PhotonNetwork.InRoom)
+                {
+                PhotonNetwork.LeaveRoom();
+                }
+            else
+                {
+                SceneManager.LoadScene("TitleScene");
+                }
+            }
+        }
+
+    // ★ Room を出終わった瞬間に呼ばれる
+    public override void OnLeftRoom()
         {
-            SceneManager.LoadScene("TitleScene");
+        // ここで Title へ戻す（←これなら GameServer → Master 完了後）
+        SceneManager.LoadScene("TitleScene");
         }
     }
-}
