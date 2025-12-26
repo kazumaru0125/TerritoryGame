0c88b5a78cdeea0e8519841e81354ee1898ca027
harchitakemaro <harchi.takemaro@gmail.com>
2025-12-26 12:41:09 +0900

    回転修正

--- Assets/Scenes/Kondo/newtestmap/firstGarden/createMaps.cs
+++ Assets/Scenes/Kondo/newtestmap/firstGarden/createMaps.cs
@@ -134,7 +134,7 @@ public class createMaps : MonoBehaviour
                                 break;
 
                                 case "13"://鬼側スタート地点
-                                Instantiate(enemyStarts, new Vector3((-4.5f + j) * scale, 0.05f, (4.5f - i) * scale), Quaternion.Euler(180f, 0f, 0f));
+                                Instantiate(enemyStarts, new Vector3((-4.5f + j) * scale, 0.0f, (4.5f - i) * scale), Quaternion.Euler(0f, 0f, 0f));
                                 break;
 
                             case "31"://横向きの策
